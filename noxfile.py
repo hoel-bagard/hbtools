cf6652b8a343429b232869008465880abb68aaf3
Hoel Bagard <hoel.bagard@gmail.com>
2023-06-04 01:15:18 +0900

    Add opencv to nox install.

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ def tests(session) -> None:  # pyright: ignore[reportMissingParameterType]
     """Run the test suite."""
     session.install("--upgrade", "pip")
     # session.install("-r", "requirements/requirements-test.txt")  # Does not work on <3.11 because of the hashes.
-    session.install("pytest")
+    session.install("pytest", "opencv-python")
     session.install(".")
     session.run("pytest", "-v", "tests")
 
