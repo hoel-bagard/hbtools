731de115bb73de94c97dd614c3818288a2147311
Hoel Bagard <hoel.bagard@gmail.com>
2024-03-18 18:50:41 +0900

    chore: bump version

--- src/hbtools/__init__.py
+++ src/hbtools/__init__.py
@@ -1,6 +1,6 @@
 """Package containing a few python utils functions."""
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from .logger import create_logger
 from .misc import clean_print, yes_no_prompt
